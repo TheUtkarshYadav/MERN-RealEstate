6dc89a90d67766c8caa7ae068aa3c4a5856ee768
Utkarsh <utkarsh8900@gmail.com>
2024-06-12 23:08:08 +0530

    update the header and make the profile page private

--- client/src/components/Navs.jsx
+++ client/src/components/Navs.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Navs = () => {
+    const { currentUser } = useSelector(state => state.user);
+
     return (
         <ul className="flex gap-10">
             <Link to='/'>
@@ -9,8 +12,12 @@ const Navs = () => {
             <Link to='/about'>
                 <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
             </Link>
-            <Link to='/sign-in'>
-                <li className="text-slate-700 hover:underline">Sign in</li>
+            <Link to='/profile'>
+                {currentUser ? (
+                    <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar} alt="profile" />
+                ) : (
+                    <li className="text-slate-700 hover:underline">Sign in</li>
+                )}
             </Link>
         </ul>
     );
