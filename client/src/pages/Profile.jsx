27f5f84ee2934e58811f3104a4950940d67725cf
Utkarsh <utkarsh8900@gmail.com>
2024-06-13 14:31:56 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,77 @@
 import { useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase.js'
 
 const Profile = () => {
+    const fileRef = useRef(null);
     const { currentUser } = useSelector((state) => state.user);
+    const [file, setFile] = useState(undefined);
+    const [filePercent, setFilePercent] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(false);
+    const [formData, setFormData] = useState({});
+
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setFilePercent(Math.round(progress));
+                console.log(filePercent);
+            },
+            (error) => {
+                setFileUploadError(true);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then(
+                    (downloadURL) => {
+                        setFormData({ ...formData, avatar: downloadURL });
+                    }
+                );
+            }
+        );
+    };
+
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file);
+        }
+    }, [file]);
+
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
             <form className='flex flex-col gap-4'>
-                <img src={currentUser.avatar} alt="Profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+                <input
+                    onChange={(e) => setFile(e.target.files[0])}
+                    type='file'
+                    ref={fileRef}
+                    hidden accept='image/*'
+                />
+
+                <img
+                    onClick={() => fileRef.current.click()}
+                    src={formData.avatar || currentUser.avatar}
+                    alt="Profile"
+                    className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+                />
+
+                <p className='text-sm self-center'>
+                    {fileUploadError ? (
+                        <span className='text-red-700'>Error uploading image (Image must be less than 2MB.)</span>
+                    ) : filePercent > 0 && filePercent < 100 ? (
+                        <span className='text-slate-700'>{`Uploading ${filePercent}%`}</span>
+                    ) : filePercent === 100 ? (
+                        <span className='text-green-600'>Image uploaded successfully!</span>
+                    ) : (
+                        ""
+                    )}
+                </p>
+
                 <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg' />
                 <input type='text' placeholder='email' id='email' className='border p-3 rounded-lg' />
                 <input type='text' placeholder='password' id='password' className='border p-3 rounded-lg' />
