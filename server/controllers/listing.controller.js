5ef9588785ec328517453109af281e2f1b24c7d0
Utkarsh <utkarsh8900@gmail.com>
2024-06-21 14:36:52 +0530

    create update listing api route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -26,3 +26,25 @@ export const deleteListing = async (req, res, next) => {
         }
     }
 }
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found.'));
+    } else if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You are not authorized to update this listing.'));
+    } else {
+        try {
+            const updatedListing = await Listing.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                { new: true }
+            );
+
+            res.status(200).json(updatedListing);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
