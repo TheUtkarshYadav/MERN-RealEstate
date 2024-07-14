be7eb2d3a48a604acb44a0f59fcbdd2ec7809276
Utkarsh <utkarsh8900@gmail.com>
2024-07-14 13:53:01 +0530

    add pre installation

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from 'path';
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
 
 dotenv.config();
@@ -15,12 +16,20 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 });
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/dist/index.js'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
